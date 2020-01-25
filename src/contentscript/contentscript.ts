8da9f9ab9737de8dcb843e1d2081c5a79416b5d4
Gopal sharma <gopal.sharma@99acres.com>
2020-01-25 08:42:35 +0530

    added json viewer

--- src/contentscript/contentscript.ts
+++ src/contentscript/contentscript.ts
@@ -1,1 +1,29 @@
+import { errorPage, jsonToHTML } from '../helpers/jsonformatter';
+import { safeStringEncodeNums } from '../helpers/safe-encode-numbers';
+import { installCollapseEventListeners } from '../helpers/collapse';
 console.log('content script')
+
+/**
+ * This script runs on every page. It communicates with the background script
+ * to help decide whether to treat the contents of the page as JSON.
+ */
+chrome.runtime.sendMessage({}, (response: boolean) => {
+  if (!response) {
+    return;
+  }
+
+  // At least in chrome, the JSON is wrapped in a pre tag.
+  const content = document.getElementsByTagName('pre')[0].innerText;
+  let outputDoc = '';
+
+  try {
+    const jsonObj = JSON.parse(safeStringEncodeNums(content));
+    outputDoc = jsonToHTML(jsonObj, document.URL);
+  } catch (e) {
+    outputDoc = errorPage(e, content, document.URL);
+  }
+
+  document.documentElement.innerHTML = outputDoc;
+  installCollapseEventListeners();
+});
+
